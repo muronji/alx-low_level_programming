32084d56af07e8fe745d34e7dcc43769c20b8743
muronji <muronjimulievi@gmail.com>
2022-09-08 12:08:28 -0700

    Betty doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
- *main-prints text
+ *main - prints using put
+ *Return: 0 if succesful
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
