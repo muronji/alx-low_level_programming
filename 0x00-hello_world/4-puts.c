0c164e42956f032028dc9dcac33b53cc0c7a4036
muronji <muronjimulievi@gmail.com>
2022-09-08 11:46:20 -0700

    Betty dtyle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+
+
+
 /**
- * program to display text
- */
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
 int main(void)
+	
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
