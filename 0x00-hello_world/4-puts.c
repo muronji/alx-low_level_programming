9b66ebd55ae2a7a7c1da66d480f19ac1a12dbc69
muronji <muronjimulievi@gmail.com>
2022-09-08 12:02:21 -0700

    Fixed code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * program to display text
+ *main-prints text
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
