fd8cc694659e7e1c4290fa09c3a711b5602f9203
muronji <muronjimulievi@gmail.com>
2022-09-09 19:05:43 -0700

    Prints alphabet in both cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - generates the alphabet in both cases
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
