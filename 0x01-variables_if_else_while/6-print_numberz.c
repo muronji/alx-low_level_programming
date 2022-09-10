ab6f042e891da7c967f6eeae1831be38d7c9f36d
muronji <muronjimulievi@gmail.com>
2022-09-09 19:33:30 -0700

    Prints numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + 0);
+	}
+	putchar('\n');
+	return (0);
+}
