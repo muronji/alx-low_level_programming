e4fa062b2fa9bfea833389e883aa47a8047bccec
muronji <muronjimulievi@gmail.com>
2022-09-14 14:43:10 -0700

    Prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * main - program that prints the putchar function
+ * return: 0 if succesful
+ */
+int main(void)
+{
+	char str = "_putchar";
+
+	
+	_putchar(str);
+	_putchar('\n');
+
+	return (0);
+}
+
