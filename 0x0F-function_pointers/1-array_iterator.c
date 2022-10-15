b4a94947cf6d0a099223153035df4b98e8dc88f6
muronji <muronjimulievi@gmail.com>
2022-10-15 07:46:32 -0700

    betty

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 
 /**
  * array_iterator - a function that executes a given function
- * 		    as a parameter on each element of an array
+ *		 as a parameter on each element of an array
  *
  * @array: array to iterate
  * @size: size of array
